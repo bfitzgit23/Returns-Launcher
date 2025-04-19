05fbd2c651d5696b8b835702757ddbf606457b0d
bfitzgit23 <dfitz@me.com>
2025-04-18 23:55:31 -0400

    added new artwork

--- scripts/preload.js
+++
@@ -1,9 +0,0 @@
-
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('huntedAPI', {
-  setAlignment: (path) => localStorage.setItem('alignmentPath', path),
-  getAlignment: () => localStorage.getItem('alignmentPath'),
-  skipIntro: () => localStorage.setItem('skipIntro', 'true'),
-  shouldSkipIntro: () => localStorage.getItem('skipIntro') === 'true'
-});
